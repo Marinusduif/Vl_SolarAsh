9ef4fb81202ecd36707e2b9e41e7972da6487760
Marinusduif <34818@ma-web.nl>
2023-12-06 10:54:53 +0100

    GoopPickUp

---
+++ VS_SolarAsh/Assets/script/pickups/GoopCollect.cs
@@ -0,0 +1,23 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class GoopCollect : MonoBehaviour
+{
+    private int goopAmount;
+
+    public TextMeshProUGUI GoopCount;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.transform.tag == ("Goop"))
+        {
+            goopAmount++;
+            GoopCount.text = goopAmount.ToString();
+            Debug.Log(goopAmount);
+            Destroy(other.gameObject);
+
+
+        }
+    }
+
+}
