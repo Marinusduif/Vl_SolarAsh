b606c7fd9f07712991a7307ae37e5a87d430b5d9
Ivo <112894315+IvoBergen@users.noreply.github.com>
2023-12-13 11:58:42 +0100

    Added TEMP sprint animation

--- VS_SolarAsh/Assets/script/player/AttackAnimation.cs
+++ VS_SolarAsh/Assets/script/player/AttackAnimation.cs
@@ -10,18 +10,29 @@ public class AttackAnimation : MonoBehaviour
 
     private void Update()
     {
-        ani.SetTrigger("Idle");
         if (Input.GetKeyDown(KeyCode.Mouse0))
         {
             ani.SetTrigger("Attack");
+            ani.SetBool("Sprinting", false);
         }
-        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D))
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D) && !Input.GetKeyDown(KeyCode.LeftShift))
         {
-            ani.SetTrigger("Walking");
+            ani.SetBool("Walking", true);
+            ani.SetTrigger("Walking1");
+            ani.SetBool("Sprinting", false);
+        }
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            ani.SetBool("Sprinting", true);
+            ani.SetBool("Walking", false);
+            ani.SetBool("Walking2", false);
         }
         else
         {
             ani.SetTrigger("Idle");
+            ani.SetBool("Sprinting", false);
+            ani.SetBool("Walking", false);
+            ani.SetBool("Walking2", false);
         }
     }
 
