fa054341d9388e42b13c48021b79e8b3b35f2848
FlorianBruijn <florian@bruijn.eu>
2023-12-19 09:56:56 +0100

    ?

---
+++ VS_SolarAsh/Assets/script/player/grind1.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class grind1 : MonoBehaviour
+{
+    private CharacterController controller;
+    private movement3 movement;
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        movement = GetComponent<movement3>();
+    }
+
+    void Update()
+    {
+
+    }
+    private void OnCollisionStay(Collision collision)
+    {
+        if (collision.gameObject.tag == "rail")
+        {
+            movement.enabled = false;
+        }
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.tag == "rail")
+        {
+            movement.enabled = true;
+        }
+    }
+}
