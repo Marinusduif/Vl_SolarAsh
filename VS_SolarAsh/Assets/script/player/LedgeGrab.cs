7ed2c854d55bd637c495df43d0b28891c3e0d1c7
Ivo <112894315+IvoBergen@users.noreply.github.com>
2024-01-19 12:50:00 +0100

    begining of ledgegrab beta

---
+++ VS_SolarAsh/Assets/script/player/LedgeGrab.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class LedgeGrab : MonoBehaviour
+{
+    [SerializeField] private string targetTag;
+    [SerializeField] private Animator ani;
+
+    private void Start()
+    {
+        ani = GetComponent<Animator>();
+    }
+    private void OnTriggerEnter(Collider coll)
+    {
+        if (coll.gameObject.tag == targetTag)
+        {
+
+            ani.SetTrigger("Climb");
+        }
+    }
+}
