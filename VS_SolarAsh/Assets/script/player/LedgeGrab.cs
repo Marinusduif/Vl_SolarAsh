8d9286fdfa82336c200efa47ba695272c4fe2b49
Ivo <112894315+IvoBergen@users.noreply.github.com>
2024-01-23 13:10:42 +0100

    Ledge grab beta
    bugs are still there but they will be patched

--- VS_SolarAsh/Assets/script/player/LedgeGrab.cs
+++ VS_SolarAsh/Assets/script/player/LedgeGrab.cs
@@ -1,19 +1,39 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
 public class LedgeGrab : MonoBehaviour
 {
     [SerializeField] private string targetTag;
     [SerializeField] private Animator ani;
+    [SerializeField] private CharacterController controller;
+    Vector3 climb = new Vector3(0, 2, 1);
+
+    bool flag = false;
 
     private void Start()
     {
         ani = GetComponent<Animator>();
+        controller = GetComponent<CharacterController>();
+
     }
+
     private void OnTriggerEnter(Collider coll)
     {
-        if (coll.gameObject.tag == targetTag)
+
+        if (coll.gameObject.tag == targetTag && !flag)
         {
-            ani.SetTrigger("Climb");
+            StartCoroutine(TimeDelay());
         }
+    }
+    IEnumerator TimeDelay()
+    {
+        flag = true;
+        ani.SetTrigger("Climb");
+
+
+        yield return new WaitForSeconds(2);
+
+        controller.Move(climb);
+        flag = false;
     }
 }
